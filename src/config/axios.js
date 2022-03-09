7c538095ee346ef65ccd2dca34dbf3afa3725752
Elías Estupiñan <eliaset096@gmail.com>
2022-03-08 20:39:16 -0500

    new scrips added

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from "axios";
 
-let instance=axios.create({
-    baseURL: "http://localhost:3001/",
-    responseType: 'json',
-    headers: {'X-Requested-Width': 'XMLHttpRequest'}
-})
+let instance = axios.create({
+  baseURL: "http://localhost:3001/",
+  responseType: "json",
+  headers: { "X-Requested-Width": "XMLHttpRequest" },
+});
 
 export default instance;
